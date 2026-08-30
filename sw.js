22e179c7141e0c6c41a77174973c6bdccd49d06c
diegodona36-netizen <diegodona36@gmail.com>
2026-08-30 00:49:44 -0400

    feat: sync v8 sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Permite que la aplicación funcione en zonas remotas de Maturín sin internet.
  */
 
-const CACHE_NAME = 'maturin-monitoreo-v7';
+const CACHE_NAME = 'maturin-monitoreo-v8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
