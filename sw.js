c8dd6c6750039478681dbe28b5d862ceb0e2b879
diegodona36-netizen <diegodona36@gmail.com>
2026-08-30 01:00:19 -0400

    fix: millimeter-precise vector pins sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Permite que la aplicación funcione en zonas remotas de Maturín sin internet.
  */
 
-const CACHE_NAME = 'maturin-monitoreo-v9';
+const CACHE_NAME = 'maturin-monitoreo-v10';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
